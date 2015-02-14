ac45990810aa30d729a92e2595ef7760b0be3bfe
Chris Van Wagoner <chrisvanwagoner@gmail.com>
2015-02-14 13:58:48 -0600

    Added initial structure

---
+++ CS4348P1/BatchProcessorMain.java
@@ -0,0 +1,26 @@
+import java.io.File; 
+import java.io.FileInputStream; 
+public class BatchProcessorMain { 
+
+public static void main (String[] args) { 
+	try { 
+		
+		String fileName = args[0]; 
+		File f = new File ("fileName"); 
+		FileInputStream fis = new FileInputStream(f); 
+		// TODO pass file to XML Parser returns array list 
+		// TODO pass array list to command builder case 1 without pipe case 2 with pipe 
+		// TODO case 1 without pipe create a new objects with the args 
+		// TODO case 2 with pipe create a new objects with the args. 
+
+
+		} 
+		
+		catch (Exception e) { 
+		System.out.println(e.getMessage()); 
+		e.printStackTrace(); 	
+		} 
+
+	} 
+
+} 
